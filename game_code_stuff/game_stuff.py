1cf53f3bac78e300d48b0a359f40f6a44e091b1f
Kyle Loggenberg <kyleloggenberg9@gmail.com>
2023-12-18 15:20:56 +0200

    Add files via upload

--- game_code_stuff/game_stuff.py
+++ game_code_stuff/game_stuff.py
@@ -1,57 +1,16 @@
 import pygame
 
 #Constants
-WIDTH, HEIGHT = 1500, 900
+WIDTH, HEIGHT = 600, 900
 
 WHITE, BLACK, RED, BLUE = (255,255,255), (0, 0, 0), (255,0,0), (0,0,255)
 
-PLAYER_SIZE_X, PLAYER_SIZE_Y = 50, 50
+PLAYER_SIZE = 50
+
 PLAYER_SPEED = 5 
 
 clock = pygame.time.Clock()
 FPS = 60
 
-def draw_border(root, WIDTH, HEIGHT):
-    pygame.draw.rect(root, WHITE ,(WIDTH/2-10, 0, 10, HEIGHT))
-    
-
-def draw_player1(root,player):
+def draw_player(root,player):
     pygame.draw.rect(root, RED, player)
-    
-    
-def draw_player2(root,player):
-    pygame.draw.rect(root, BLUE, player)
-
-
-#Move functions for player1
-def player1_move_x_increase(x):
-    if x+PLAYER_SPEED < WIDTH/2-10-PLAYER_SIZE_X:
-        return x+PLAYER_SPEED
-    return x
-
-def player1_move_x_decrease(x):
-    if x-PLAYER_SPEED > 0:
-        return x-PLAYER_SPEED
-    return x
-
-def player_move_y_increase(y):
-    if y-PLAYER_SPEED > 0:
-        return y-PLAYER_SPEED
-    return y
-
-def player_move_y_decrease(y):
-    if y+PLAYER_SPEED < HEIGHT-PLAYER_SIZE_Y:
-        return y+PLAYER_SPEED
-    return y
-
-
-#Move functions for player2
-def player2_move_x_increase(x):
-    if x+PLAYER_SPEED < WIDTH-PLAYER_SIZE_X:
-        return x+PLAYER_SPEED
-    return x
-
-def player2_move_x_decrease(x):
-    if x-PLAYER_SPEED > WIDTH/2:
-        return x-PLAYER_SPEED
-    return x
--- main.py
+++ main.py
@@ -1,119 +1,34 @@
 import pygame
 
-from game_code_stuff import game_stuff as stuff
-from game_code_stuff import check_wins as wins
-from game_code_stuff import projectiles as pro
+import game_code_stuff.game_stuff as stuff
 
-def run_game():
+def main():
     pygame.init()
     
-    #List to store arrays
-    projectile_list = []
-    
-    #Make root screen
     root = pygame.display.set_mode((stuff.WIDTH, stuff.HEIGHT))
+    pygame.display.set_caption("My Game")
     
-    #Creating player rects
-    player1 = pygame.Rect(20,stuff.HEIGHT/2-25,stuff.PLAYER_SIZE_X,stuff.PLAYER_SIZE_Y)
-    player2 = pygame.Rect(stuff.WIDTH-20-stuff.PLAYER_SIZE_X,stuff.HEIGHT/2-25,stuff.PLAYER_SIZE_X,stuff.PLAYER_SIZE_Y)
+    player = pygame.Rect(stuff.WIDTH/2-25, stuff.HEIGHT-50-25, stuff.PLAYER_SIZE, stuff.PLAYER_SIZE)
     
-    #Control Shoot
-    z_pressed = False
-    b_pressed = False
-    
-    #Main game loop
     run_game = True
     while run_game:
-        #Cap the speed of the loop
         stuff.clock.tick(stuff.FPS)
         
-        #Event Loop
         for event in pygame.event.get():
-            #Quit the game
             if event.type == pygame.QUIT:
                 run_game = False
                 break
-            #Handles Shooting Events
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_z and z_pressed == False:  # Change this to the key you want to check
-                    projectile = pro.Projectiles(player1.center, "right")
-                    projectile_list.append(projectile)
-                    z_pressed = True
-                if event.key == pygame.K_b and b_pressed == False:  # Change this to the key you want to check
-                    projectile = pro.Projectiles(player2.center, "left")
-                    projectile_list.append(projectile)
-                    b_pressed = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_z and z_pressed == True:  # Change this to the key you want to check
-                    z_pressed = False
-                if event.key == pygame.K_b and b_pressed == True:  # Change this to the key you want to check
-                    b_pressed = False
-                    
-        #Rest of the stuff
         root.fill(stuff.BLACK)
         
-        #Draw border
-        stuff.draw_border(root,stuff.WIDTH, stuff.HEIGHT)
+        stuff.draw_player(root,player)
         
-        #Draw players
-        stuff.draw_player1(root, player1)
-        stuff.draw_player2(root,player2)
-
-        #Draw Projectiles
-        for ob in projectile_list:
-            ob.draw(root)
-            
-        #Move projectiles across screen
-        for ob in projectile_list:
-            ob.x = ob.move_projectile()
-        
-        #Remove projectile if off screen
-        for ob in projectile_list:
-            if ob.check_projectile_off_screen():
-                projectile_list.remove(ob)
-        
-        # Check for key presses
-        keys = pygame.key.get_pressed()
-
-        #Movement fo player1
-        #Moving on x-axis
-        if keys[pygame.K_g]:
-            player1.x = stuff.player1_move_x_increase(player1.x)
-        if keys[pygame.K_d]:
-            player1.x = stuff.player1_move_x_decrease(player1.x)
-        #Moving on y-axis
-        if keys[pygame.K_r]:
-            player1.y = stuff.player_move_y_increase(player1.y)
-        if keys[pygame.K_f]:
-            player1.y = stuff.player_move_y_decrease(player1.y)
-        
-        #Movement fo player2
-        #Moving on x-axis
-        if keys[pygame.K_l]:
-            player2.x = stuff.player2_move_x_increase(player2.x)
-        if keys[pygame.K_j]:
-            player2.x = stuff.player2_move_x_decrease(player2.x)
-        #Moving on y-axis
-        if keys[pygame.K_i]:
-            player2.y = stuff.player_move_y_increase(player2.y)
-        if keys[pygame.K_k]:
-            player2.y = stuff.player_move_y_decrease(player2.y)
-        
-            
-        #Change var if player shots
-        if z_pressed:
-            z_pressed = False
-        if b_pressed:
-            z_pressed = False
-        
-        #Health checks
-        if wins.check_win(wins.player1_health, wins.player2_health):
-            run_game = False
-            break
-        
-        #Update screen
         pygame.display.update()
     pygame.quit()
 
-if __name__ == "__main__":
-    run_game()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
