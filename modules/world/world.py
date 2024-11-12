04c4fda24333d10027db1769e00d33df905a8daa
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-11-12 18:14:09 +0200

    Adding doc strings to world.py

--- modules/world/world.py
+++ modules/world/world.py
@@ -1,9 +1,11 @@
 import pygame
 
-#Constants
-WIDTH, HEIGHT = 800, 800
+# Constants for the screen dimensions and colors
+WIDTH, HEIGHT = 800, 800  # Dimensions of the game window
+WHITE, BLACK, RED, BLUE = (255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 0, 255)  # RGB color definitions
 
-WHITE, BLACK, RED, BLUE = (255,255,255), (0, 0, 0), (255,0,0), (0,0,255)
-
+# Clock for controlling the game's frame rate
 clock = pygame.time.Clock()
+
+# Frames per second setting for the game loop
 FPS = 60
